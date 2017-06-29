e23db0ebc62cb1fb4a99444afece0ea864335ec4
subji <qkrwlrjq@gmail.com>
2017-06-29 11:27:12 +0900

    20170629112712

--- dist/js/biochart.js
+++ dist/js/biochart.js
@@ -989,58 +989,6 @@ var draw = (function (draw)	{
 		
 		return data;
 	};
-
-	function tabInput (id, idx)	{
-		var input = document.createElement('input');
-				input.id = id;
-				input.name = 'tabs';
-				input.type = 'radio';
-				input.checked = idx === 0 ? true : false;
-
-		return input;
-	};
-
-	function tabLabel (id, name)	{
-		var label = document.createElement('label');
-				label.htmlFor = id;
-				label.innerHTML = name;
-
-		return label;
-	};
-
-	function tabContent (id, content)	{
-		var div = document.createElement('div');
-				div.id = id + '_content';
-
-		return div;
-	};
-
-	draw.tab = function (target, tabNames, tabIds)	{
-		document.querySelector(target).innerHTML = '';
-
-		var div = document.querySelector(target);
-
-		if (tabNames.length !== tabIds.length)	{
-			throw new Error('탭 제목과 ID 의 길이가 같지 않습니다.');
-		}
-
-		for (var i = 0, l = tabNames.length; i < l; i++)	{
-			var name = tabNames[i],
-					id = tabIds[i];
-
-			div.appendChild(tabInput(id, i));
-			div.appendChild(tabLabel(id, name));
-		}
-
-		for (var i = 0, l = tabNames.length; i < l; i++)	{
-			var area = tabContent(tabIds[i]);
-
-			area.appendChild(tabContent(tabIds[i] + '_chart'));
-			area.appendChild(tabContent(tabIds[i] + '_table'))
-			div.appendChild(area);
-		}
-	};
-
 	/*
 		svg 의 가로, 세로 길이를 반환해주는 함수.
 	 */
@@ -3633,7 +3581,10 @@ var survival = (function (survival)	{
 	'use strict';
 
 	var model = {};
-	
+	/*
+		Survival 데이터 (case_id, months, status) 를 구하는 
+		함수.
+	 */
 	function getSurvivalData (data)	{
 		var month = {os: [], dfs: []},
 				pure = {os: [], dfs: []},
@@ -3678,12 +3629,68 @@ var survival = (function (survival)	{
 			all: all,
 		};
 	};
+	/*
+		Tab 태그의 input 을 만드는 함수.
+	 */
+	function tabInput (id, idx)	{
+		var input = document.createElement('input');
+				input.id = id + '_survival';
+				input.name = 'tabs';
+				input.type = 'radio';
+				input.checked = idx === 0 ? true : false;
+
+		return input;
+	};
+	/*
+		Tab 태그의 라벨 즉, 탭의 제목을 그리는 함수.
+	 */
+	function tabLabel (id, name)	{
+		var label = document.createElement('label');
+				label.htmlFor = id + '_survival';
+				label.innerHTML = name;
+
+		return label;
+	};
+	/*
+		선택된 탭의 내용이 들어갈 바탕 div 를 만드는 함수.
+	 */
+	function tabContent (id, content)	{
+		var div = document.createElement('div');
+				div.id = id;
+
+		return div;
+	};
+	/*
+		Survival 탭을 만드는 함수.
+	 */
+	function makeTab (target, tabNames)	{
+		document.querySelector(target).innerHTML = '';
+
+		var div = document.querySelector(target);
+
+		for (var i = 0, l = tabNames.length; i < l; i++)	{
+			var name = tabNames[i],
+					id = tabNames[i].toLowerCase();
+
+			div.appendChild(tabInput(id, i));
+			div.appendChild(tabLabel(id, name));
+		}
+
+		for (var i = 0, l = tabNames.length; i < l; i++)	{
+			var area = tabContent(tabNames[i].toLowerCase());
+
+			area.appendChild(
+				tabContent(tabNames[i].toLowerCase() + '_survival_curve'));
+			area.appendChild(
+				tabContent(tabNames[i].toLowerCase() + '_stat_table'))
+			div.appendChild(area);
+		}
+	};
 
 	return function (o)	{
-		draw.tab(o.element, ['OS', 'DFS'], 
-		['osSurvival', 'dfsSurvival']);
-
 		var pureData = getSurvivalData(o.data).pure;
+
+		makeTab(o.element, ['OS', 'DFS'])
 
 		SurvivalTab.init(o.divisionData, pureData);
 	};
