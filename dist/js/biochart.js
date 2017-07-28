3b3d2d624be4b7432b8caadb3decfebbd22601d6
subji <qkrwlrjq@gmail.com>
2017-07-28 16:22:35 +0900

    20170728162235

--- dist/js/biochart.js
+++ dist/js/biochart.js
@@ -6910,7 +6910,7 @@ var render = (function (render)	{
 					drag.substring(0, 1).toUpperCase() + 
 					drag.substring(1) : drag;
 
-			console.log(dg, nm)
+			console.log(dg, nm, d3.behavior.drag().on())
 
 			dg.on(nm, drags[drag]);
 		}
