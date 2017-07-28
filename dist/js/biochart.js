ee2c7fe46b435dd77fbff2db206154c49227ac10
subji <qkrwlrjq@gmail.com>
2017-07-28 17:16:34 +0900

    20170728171634

--- dist/js/biochart.js
+++ dist/js/biochart.js
@@ -2182,7 +2182,7 @@ config.expression.sample = {
 
 			return d.text.indexOf('Low') > -1 ? 
 						 bcr.height / 2 + (bcr.height / 4) : 
-						 draw.getTextHeight('25x').height / 2;
+						 draw.getTextHeight('14px').height;
 		},
 	},
 	style: {
