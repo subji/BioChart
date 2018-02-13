f072416c15e46c957f09e2f76148dc2595a60c8c
unknown <qkrwlrjq@gmail.com>
2018-02-13 11:20:41 +0900

    '20180213-3'

--- public/javascripts/test/index.js
+++ public/javascripts/test/index.js
@@ -97,38 +97,38 @@
  /*
     Landscape
   */
- $.ajax({
-    'type': 'POST',
-    'url': '/files/datas',
-    data: {
-     name: 'landscape',
-    },
-    beforeSend: function () {
-      // bio.loading().start(document.querySelector('#main'), 900, 600);
-    },
-    success: function (d) {
-      bio.landscape({
-				element: '#main',
-				width: 1200,
-				height: 800,
-				data: {
-					pq: 'p',
-					type: 'LUAD',
-					data: d[0].data,
-					title:d[0].data.name,
-				},
-        plot: {
-          patient: false, // true
-          pq: false, // true
-        },
-        divisionFunc: function (enable, disable, others)  {
-          // console.log('DD');
-        },
-			});
+ // $.ajax({
+ //    'type': 'POST',
+ //    'url': '/files/datas',
+ //    data: {
+ //     name: 'landscape',
+ //    },
+ //    beforeSend: function () {
+ //      // bio.loading().start(document.querySelector('#main'), 900, 600);
+ //    },
+ //    success: function (d) {
+ //      bio.landscape({
+	// 			element: '#main',
+	// 			width: 1200,
+	// 			height: 800,
+	// 			data: {
+	// 				pq: 'p',
+	// 				type: 'LUAD',
+	// 				data: d[0].data,
+	// 				title:d[0].data.name,
+	// 			},
+ //        plot: {
+ //          patient: false, // true
+ //          pq: false, // true
+ //        },
+ //        divisionFunc: function (enable, disable, others)  {
+ //          // console.log('DD');
+ //        },
+	// 		});
 
-      // bio.loading().end();
-    },
-  });
+ //      // bio.loading().end();
+ //    },
+ //  });
 
 
 /* Variants */
